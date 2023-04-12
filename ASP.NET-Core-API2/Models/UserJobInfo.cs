312a83f044474870c67c608d412a66d0495a4db9
altair <ar80pro@gmail.com>
2023-04-12 22:46:10 +0200

    Adding Models mirroring the Db Tables

---
+++ ASP.NET-Core-API2/Models/UserJobInfo.cs
@@ -0,0 +1,9 @@
+﻿namespace ASP.NET_Core_API2.Models
+{
+    public class UserJobInfo
+    {
+        public int UserId { get; set; }
+        public string JobTitle { get; set; } = "";
+        public string Department { get; set; } = "";
+    }
+}
---
+++ ASP.NET-Core-API2/Models/UserSalary.cs
@@ -0,0 +1,8 @@
+﻿namespace ASP.NET_Core_API2.Models
+{
+    public class UserSalary
+    {
+        public int UserId { get; set; }
+        public decimal Salary { get; set; }
+    }
+}
