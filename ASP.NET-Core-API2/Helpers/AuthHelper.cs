dd50f611f1effdd4a99086d2a388322801e0e74c
altair <ar80pro@gmail.com>
2023-04-15 21:59:09 +0200

    Moving Helper Methods Out Of AuthController

---
+++ ASP.NET-Core-API2/Helpers/AuthHelper.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace ASP.NET_Core_API2.Helpers
+{
+    public class AuthHelper
+    {
+        private readonly IConfiguration _config;
+        public AuthHelper(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public byte[] GetPasswordHash(string password, byte[] passwordSalt)
+        {
+            string passwordSaltPlusString = _config.GetSection("AppSettings:PasswordKey").Value +
+                Convert.ToBase64String(passwordSalt);
+
+            return KeyDerivation.Pbkdf2(
+                password: password,
+                salt: Encoding.ASCII.GetBytes(passwordSaltPlusString),
+                prf: KeyDerivationPrf.HMACSHA256,
+                iterationCount: 1000000,
+                numBytesRequested: 256 / 8
+            );
+        }
+
+        public string CreateToken(int userId)
+        {
+            string? tokenKeyString = _config.GetSection("AppSettings:TokenKey").Value;
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(tokenKeyString ?? "");
+            var descriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+               {
+                    new Claim("userId", userId.ToString())
+               }),
+                Expires = DateTime.UtcNow.AddDays(7),
+                SigningCredentials = new(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = tokenHandler.CreateToken(descriptor);
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
