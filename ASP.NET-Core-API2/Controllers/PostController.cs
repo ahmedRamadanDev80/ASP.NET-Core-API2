e4ddcdccbff5521736cfaf71047922355263bf4b
altair <ar80pro@gmail.com>
2023-04-16 17:20:03 +0200

    Adding Get Action Methods To PostController

---
+++ ASP.NET-Core-API2/Controllers/PostController.cs
@@ -0,0 +1,118 @@
+﻿using ASP.NET_Core_API2.Data;
+using ASP.NET_Core_API2.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASP.NET_Core_API2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PostController : ControllerBase
+    {
+        private readonly DataContextDapper _dapper;
+        public PostController(IConfiguration config)
+        {
+            _dapper = new DataContextDapper(config);
+        }
+
+        // ---------- GET ALL ----------
+        [HttpGet("GetAll")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public ActionResult<IEnumerable<Post>> GetPosts()
+        {
+            string sql = @"SELECT [PostId],
+                    [UserId],
+                    [PostTitle],
+                    [PostContent],
+                    [PostCreated],
+                    [PostUpdated] 
+                FROM TutorialAppSchema.Posts";
+            try
+            {
+                IEnumerable<Post> posts = _dapper.LoadData<Post>(sql);
+                return Ok(posts);
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        // ---------- GET BY ID ----------
+        [HttpGet("Get/{postId}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public ActionResult<Post> GetPost(int postId)
+        {
+            string sql = @"SELECT [PostId],
+                    [UserId],
+                    [PostTitle],
+                    [PostContent],
+                    [PostCreated],
+                    [PostUpdated] 
+                FROM TutorialAppSchema.Posts
+                    WHERE PostId = " + postId.ToString();
+            try
+            {
+                Post post = _dapper.LoadDataSingle<Post>(sql);
+                return Ok(post);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        // ---------- GET BY USER ----------
+        [HttpGet("Get/{userId}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public ActionResult<IEnumerable<Post>> GetUserPosts(int userId)
+        {
+            string sql = @"SELECT [PostId],
+                    [UserId],
+                    [PostTitle],
+                    [PostContent],
+                    [PostCreated],
+                    [PostUpdated] 
+                FROM TutorialAppSchema.Posts
+                    WHERE UserId = " + userId.ToString();
+            try
+            {
+                IEnumerable<Post> posts = _dapper.LoadData<Post>(sql);
+                return Ok(posts);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        // ---------- GET MY POSTS ----------
+        [HttpGet("GetMyPosts")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public ActionResult<IEnumerable<Post>> GetMyPosts()
+        {
+            string sql = @"SELECT [PostId],
+                    [UserId],
+                    [PostTitle],
+                    [PostContent],
+                    [PostCreated],
+                    [PostUpdated] 
+                FROM TutorialAppSchema.Posts
+                    WHERE UserId = " + this.User.FindFirst("userId")?.Value;
+            try
+            {
+                IEnumerable<Post> posts = _dapper.LoadData<Post>(sql);
+                return Ok(posts);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+    }
+}
