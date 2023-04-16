2ce0cdc2ee24a75ab1ac704a8e22101c783befe5
altair <ar80pro@gmail.com>
2023-04-16 16:41:29 +0200

    Post DTO Models

---
+++ ASP.NET-Core-API2/Models/Dtos/PostToAddDto.cs
@@ -0,0 +1,8 @@
+﻿namespace ASP.NET_Core_API2.Models.Dtos
+{
+    public class PostToAddDto
+    {
+        public string PostTitle { get; set; } = "";
+        public string PostContent { get; set; } = "";
+    }
+}
---
+++ ASP.NET-Core-API2/Models/Dtos/PostToEditDto.cs
@@ -0,0 +1,9 @@
+﻿namespace ASP.NET_Core_API2.Models.Dtos
+{
+    public class PostToEditDto
+    {
+        public int PostId { get; set; }
+        public string PostTitle { get; set; } = "";
+        public string PostContent { get; set; } = "";
+    }
+}
