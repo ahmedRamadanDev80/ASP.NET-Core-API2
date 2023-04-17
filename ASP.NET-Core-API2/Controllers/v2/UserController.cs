becff4fe23a4bf094f1da69b8fa426b6bb7be088
altair <ar80pro@gmail.com>
2023-04-17 22:18:47 +0200

    GetUsers V2 EndPoint

---
+++ ASP.NET-Core-API2/Controllers/v2/UserController.cs
@@ -0,0 +1,59 @@
+﻿using ASP.NET_Core_API2.Data;
+using ASP.NET_Core_API2.Models;
+using ASP.NET_Core_API2.Models.Dtos;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASP.NET_Core_API2.Controllers.v2
+{
+    [Authorize]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    [ApiController]
+    [ApiVersion("2.0")]
+    public class UserController : ControllerBase
+    {
+        private readonly DataContextDapper _dapper;
+        public UserController(IConfiguration config)
+        {
+            _dapper = new DataContextDapper(config);
+        }
+
+        // ---------- GET ----------
+        [HttpGet("GetUsers/{userId}/{isActive}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public ActionResult<IEnumerable<UserV2>> GetUsers(int userId, bool isActive)
+        {
+            string sql = @"EXEC TutorialAppSchema.spUsers_Get";
+            string parameters = "";
+
+            if (userId != 0)
+            {
+                parameters += ", @UserId=" + userId.ToString();
+            }
+            if (isActive)
+            {
+                parameters += ", @Active=" + isActive.ToString();
+            }
+
+            if (parameters.Length > 0)
+            {
+                sql += parameters.Substring(1);
+            }
+            try
+            {
+                IEnumerable<UserV2> users = _dapper.LoadData<UserV2>(sql);
+                return Ok(users);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        
+
+
+    }
+}
